b14ad1bcf29461297b9a8ed553a703cc2363ba63
chiaming yen <chiamingyen@gmail.com>
2014-12-03 12:03:05 +0800

    cherrypy

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['CherryPy', 'mako'],
+    install_requires=['CherryPy'],
     # install_requires=['Django>=1.3'],
 )
